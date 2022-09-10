704c3956097458d16317d0c2e5a31e8f74948969
Aapuji <om.robotics16@gmail.com>
2022-09-09 22:54:17 -0400

    Fixed problems with wierd layout of multi-article. Also put it a default image of a square for those imgs. Also started work on footer, and chose a color, though it might not be the final color.

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,14 @@
 'use strict'
 
+const ROOT = document.querySelector(':root');
+
 // Function for when JOIN button is clicked
 const log = () => console.log('CLICKED!');
+
+// Function to change the heights of images to be square
+const squareImg = imgs => {
+    for (let img of imgs) {
+        const width = (getComputedStyle(img).width);
+        ROOT.style.setProperty('--home-multi-img-height', width);
+    }
+}
